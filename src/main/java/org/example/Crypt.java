aa903a9a18783cbdc9a4c5673209d4e17be9c90b
Th3R3v1v3r <iddifuad01@gmail.com>
2023-03-16 16:11:15 +0000

    beggining of proejct

--- src/main/java/org/example/Crypt.java
+++ src/main/java/org/example/Crypt.java
@@ -3,6 +3,6 @@ package org.example;
 public class Crypt {
     public static void main(String[] args) {
         System.out.println("hi");
-        System.out.println("123");
+        System.out.println("123")
     }
 }
