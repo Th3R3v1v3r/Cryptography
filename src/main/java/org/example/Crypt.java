7c6ae682e9783f23af89cc5860a712f6345d2ba8
Th3R3v1v3r <iddifuad01@gmail.com>
2023-03-20 11:02:24 +0000

    beggining of proejct

--- src/main/java/org/example/Crypt.java
+++ src/main/java/org/example/Crypt.java
@@ -1,8 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Crypt {
+    
     public static void main(String[] args) {
-        System.out.println("Please create a Master Password");
+
+
+
+        System.out.println("This is a program to store Id and password Pairs\n" +
+                "If you would you like to add a pair, Please press 1\n" +
+                "If you would you like to edit a pair, Please press 2\n" +
+                "If you would you like to remove a pair, Please press 3");
+
+        Scanner s = new Scanner(System.in);
+        int choice = Integer.valueOf(s.nextLine());
+        switch(choice){
+            case 1:
+                System.out.println("If you would you like to add a pair, Please press 1\n");
+
+            }
+        }
+
+
 
     }
-}
+
